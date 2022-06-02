25cff905363d5e1d9ec0c8d7a8a996cc0fbae172
abhinaya-sinha <asinha15@illinois.edu>
2022-06-02 17:26:57 -0500

    a

--- train_model.py
+++ train_model.py
@@ -89,7 +89,7 @@ if __name__ == "__main__":
     ax = fig.add_subplot(1,1,1)
     ax.plot(np.linspace(0, epochs, epochs), losses, label = 'train loss')
     ax.plot(np.linspace(0,epochs, epochs), test_losses, label = 'test loss')
-    plt.yscale('log')
+    ax.set_yscale('log')
     ax.legend()
     fig.savefig('plot.png')
     plt.show(block=True)
